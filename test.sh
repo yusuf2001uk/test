440c03623fb136cd604d0558e011f3b415b26603
yusuf2001uk <yusuf2001uk@gmail.com>
2024-08-21 19:48:03 +0300

    Delete test.sh

--- test.sh
+++
@@ -1,1 +0,0 @@
-docker run cloudflare/cloudflared:latest tunnel --no-autoupdate run --token eyJhIjoiNTFkZDM1ZjFjMDYzNzc0ODI5OTcwMTIzZWU3ZTFlNDUiLCJ0IjoiNzk4ZDQyYjAtYTgxMC00MDg3LWFhZmUtOWU0MWE5OGIwOTU5IiwicyI6Ik1URmxNR1UwTWpZdFlqZG1OaTAwWm1aakxUaGhORFl0TVRFek9UVTNaREUxTVdNeiJ9
