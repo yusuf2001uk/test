d56cc98f97dd07ce8fdcbb108ecc28979e7b1f9f
yusuf2001uk <yusuf2001uk@gmail.com>
2024-08-21 19:44:23 +0300

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,1 @@
+docker run cloudflare/cloudflared:latest tunnel --no-autoupdate run --token eyJhIjoiNTFkZDM1ZjFjMDYzNzc0ODI5OTcwMTIzZWU3ZTFlNDUiLCJ0IjoiNzk4ZDQyYjAtYTgxMC00MDg3LWFhZmUtOWU0MWE5OGIwOTU5IiwicyI6Ik1URmxNR1UwTWpZdFlqZG1OaTAwWm1aakxUaGhORFl0TVRFek9UVTNaREUxTVdNeiJ9
